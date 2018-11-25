40e2c76b341bbb37d05b25159d795c52eb303415
NickMurr <32221055+NickMurr@users.noreply.github.com>
2018-11-25 14:40:53 +0200

    Links, Params and Redirect

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -3,19 +3,35 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 import uuid from 'uuid';
+import TextInputGroup from '../layout/TextInputGroup';
 import { Consumer } from '../../context';
 
 class AddContact extends Component {
   state = {
     name: '',
     email: '',
-    phone: ''
+    phone: '',
+    errors: {}
   };
 
   onSubmit = (dispatch, e) => {
     e.preventDefault();
 
     const { name, email, phone } = this.state;
+
+    // Check For Errors
+    if (name === '') {
+      this.setState({ errors: { name: 'Phone is required' } });
+      return;
+    }
+    if (email === '') {
+      this.setState({ errors: { email: 'Phone is required' } });
+      return;
+    }
+    if (phone === '') {
+      this.setState({ errors: { phone: 'Phone is required' } });
+      return;
+    }
     const newContact = {
       id: uuid(),
       name,
@@ -27,14 +43,17 @@ class AddContact extends Component {
     this.setState({
       name: '',
       email: '',
-      phone: ''
+      phone: '',
+      errors: {}
     });
+
+    this.props.history.push('/');
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
 
   render() {
-    const { name, email, phone } = this.state;
+    const { name, email, phone, errors } = this.state;
 
     return (
       <Consumer>
@@ -45,39 +64,31 @@ class AddContact extends Component {
               <div className="card-header">Add Contact</div>
               <div className="card-body">
                 <form onSubmit={this.onSubmit.bind(this, dispatch)}>
-                  <div className="form-group">
-                    <label htmlFor="name">Name</label>
-                    <input
-                      type="text"
-                      name="name"
-                      className="form-control form-control-lg"
-                      placeholder="Enter Name..."
-                      value={name}
-                      onChange={this.onChange}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input
-                      type="email"
-                      name="email"
-                      className="form-control form-control-lg"
-                      placeholder="Enter Email..."
-                      value={email}
-                      onChange={this.onChange}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="phone">Phone</label>
-                    <input
-                      type="number"
-                      name="phone"
-                      className="form-control form-control-lg"
-                      placeholder="Enter Phone..."
-                      value={phone}
-                      onChange={this.onChange}
-                    />
-                  </div>
+                  <TextInputGroup
+                    label="Name"
+                    name="name"
+                    placeholder="Enter Name"
+                    value={name}
+                    onChange={this.onChange}
+                    error={errors.name}
+                  />
+                  <TextInputGroup
+                    label="Email"
+                    name="email"
+                    placeholder="Enter Email"
+                    value={email}
+                    onChange={this.onChange}
+                    type="email"
+                    error={errors.email}
+                  />
+                  <TextInputGroup
+                    label="Phone"
+                    name="phone"
+                    placeholder="Enter Phone"
+                    value={phone}
+                    onChange={this.onChange}
+                    error={errors.phone}
+                  />
                   <input
                     type="submit"
                     value="Add Contact"
--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -11,6 +11,12 @@ class Contacts extends Component {
           const { contacts } = value;
           return (
             <React.Fragment>
+              <h1 className="display-4 mb-4">
+                <span className="text-danger">
+                  <strong>Contact </strong>
+                </span>
+                List
+              </h1>
               {contacts.map(contact => (
                 <Contact key={contact.id} contact={contact} />
               ))}
