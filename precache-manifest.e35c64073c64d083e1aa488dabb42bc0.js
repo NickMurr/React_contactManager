b7ccb908a9bf3a3e10a05727e7c8b0f199a04041
NickMurr <32221055+NickMurr@users.noreply.github.com>
2018-11-26 10:32:27 +0200

    Updates

---
+++ precache-manifest.e35c64073c64d083e1aa488dabb42bc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "784306108a20a3199cfc",
+    "url": "/React_contactManager/static/js/runtime~main.78430610.js"
+  },
+  {
+    "revision": "9fdef9fcb8cdca502568",
+    "url": "/React_contactManager/static/js/main.9fdef9fc.chunk.js"
+  },
+  {
+    "revision": "5b3647171b59905f8992",
+    "url": "/React_contactManager/static/js/1.5b364717.chunk.js"
+  },
+  {
+    "revision": "9fdef9fcb8cdca502568",
+    "url": "/React_contactManager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "5b3647171b59905f8992",
+    "url": "/React_contactManager/static/css/1.d4d11a7d.chunk.css"
+  },
+  {
+    "revision": "d1ee8d45805a5e544aefc4dd8b15b0d3",
+    "url": "/React_contactManager/index.html"
+  }
+];
